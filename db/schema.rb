47a5aa0d0379d1c433bff4aafb5dd0601a5264dd
dguajardo <diego.guajardo@hotmail.cl>
2017-10-21 16:38:15 -0300

    Devise model user and configuratios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021192013) do
+ActiveRecord::Schema.define(version: 20171021193518) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -59,6 +59,23 @@ ActiveRecord::Schema.define(version: 20171021192013) do
     t.index ["currency_id"], name: "index_movements_on_currency_id"
     t.index ["movement_type_id"], name: "index_movements_on_movement_type_id"
     t.index ["user_id"], name: "index_movements_on_user_id"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
